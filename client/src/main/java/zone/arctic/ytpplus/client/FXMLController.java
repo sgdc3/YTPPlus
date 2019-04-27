4b3711d0ee211a448a2648abfcee3c39229bc17d
Gabriele C <sgdc3.mail@gmail.com>
2019-04-27 02:33:25 +0200

    Project structure
    Co-Authored-By: philosophofee <hasben2345@gmail.com>

---
+++ client/src/main/java/zone/arctic/ytpplus/client/FXMLController.java
@@ -0,0 +1,433 @@
+package zone.arctic.ytpplus.client;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Control;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javax.swing.DefaultListModel;
+import zone.arctic.ytpplus.YTPGenerator;
+
+public class FXMLController {
+// <editor-fold defaultstate="collapsed" desc="idk how to neaten this so heres a fold to make it neater">
+    @FXML
+    private MediaView mediaviewVideoPlayer;
+
+    @FXML
+    private CheckBox cbEffect1;
+
+    @FXML
+    private CheckBox cbEffect2;
+
+    @FXML
+    private CheckBox cbEffect3;
+
+    @FXML
+    private CheckBox cbEffect4;
+
+    @FXML
+    private CheckBox cbEffect5;
+
+    @FXML
+    private CheckBox cbEffect6;
+
+    @FXML
+    private CheckBox cbEffect7;
+
+    @FXML
+    private CheckBox cbEffect8;
+
+    @FXML
+    private CheckBox cbEffect9;
+
+    @FXML
+    private CheckBox cbEffect10;
+
+    @FXML
+    private CheckBox cbEffect11;
+
+    @FXML
+    private Button btnCreate;
+
+    @FXML
+    private TextField tfClipCount;
+
+    @FXML
+    private TextField tfMaxStream;
+
+    @FXML
+    private TextField tfMinStream;
+
+    @FXML
+    private ProgressBar barProgress;
+
+    @FXML
+    private Button btnPlayVideo;
+
+    @FXML
+    private Button btnPauseVideo;
+
+    @FXML
+    private Button restartVideo;
+
+    @FXML
+    private TextField tfFFMPEG;
+
+    @FXML
+    private TextField tfFFPROBE;
+
+    @FXML
+    private TextField tfMAGICK;
+
+    @FXML
+    private TextField tfTEMP;
+
+    @FXML
+    private TextField tfSOUNDS;
+
+    @FXML
+    private TextField tfMUSIC;
+
+    @FXML
+    private TextField tfRESOURCES;
+
+    @FXML
+    private Button btnBrowseFFMPEG;
+
+    @FXML
+    private Button btnBrowseFFPROBE;
+
+    @FXML
+    private Button btnBrowseMAGICK;
+
+    @FXML
+    private Button btnBrowseTEMP;
+
+    @FXML
+    private Button btnBrowseSOUNDS;
+
+    @FXML
+    private Button btnBrowseMUSIC;
+
+    @FXML
+    private Button btnBrowseRESOURCES;
+
+    @FXML
+    private Button btnHelpMeConfig;
+
+    @FXML
+    private ListView<String> listviewSourcesList;
+
+    @FXML
+    private TextField tfSOURCES;
+
+    @FXML
+    private Button btnBrowseSOURCES;
+
+    @FXML
+    private CheckBox cbUseTransitions;
+
+    @FXML
+    private Button btnSaveAs;
+
+    @FXML
+    private Button btnAddSource;
+
+    @FXML
+    private Button btnRemoveSource;
+// </editor-fold>
+    
+    //javafx sucks. It's got a lot under the hood but it sucks.
+    //This is incredibly messy. And I can't fix it because javafx sucks.
+    //Moral of this story is don't use javafx. Swing is your friend.
+    
+    ObservableList<String> sourceList = FXCollections.observableArrayList();
+    
+    String TEMP = "temp/";
+    //Media m;
+    
+    @FXML
+    void addSource(ActionEvent event) {
+        FileChooser.ExtensionFilter fileFilter = new FileChooser.ExtensionFilter("Video files (*.mp4)", "*.mp4");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(fileFilter);
+        fileChooser.setTitle("Choose Source");
+        fileChooser.setInitialDirectory(LAST_BROWSED);
+        List<File> selected = fileChooser.showOpenMultipleDialog(null);
+        if (selected==null) return;
+        for (File file : selected) {
+            sourceList.add(file.getAbsolutePath().replace('\\', '/'));
+        }
+        listviewSourcesList.setItems(sourceList);
+        
+    }
+
+    @FXML
+    void goNow(ActionEvent event) throws Exception {
+        if (sourceList.isEmpty()) {
+            alert("You need some sources...");
+            return;
+        }
+        
+        Thread vidThread = new Thread() {
+            public void run() {
+                try {
+                btnCreate.setDisable(true);
+                System.out.println("poop");
+                YTPGenerator generator = new YTPGenerator(TEMP + "tempoutput.mp4");
+                System.out.println("poop2");
+                generator.toolBox.FFMPEG = "\"" + tfFFMPEG.getText() + "\"";
+                generator.toolBox.FFPROBE = "\"" + tfFFPROBE.getText() + "\"";
+                generator.toolBox.MAGICK = "\"" + tfMAGICK.getText() + "\"";
+                System.out.println("poop3");
+                String jobDir = tfTEMP.getText() + "job_" + System.currentTimeMillis() + "/";
+                generator.toolBox.TEMP = jobDir;
+                new File(jobDir).mkdir();
+                new File(generator.toolBox.TEMP).mkdir();
+                generator.toolBox.SOUNDS = tfSOUNDS.getText();
+                generator.toolBox.MUSIC = tfMUSIC.getText();
+                generator.toolBox.RESOURCES = tfRESOURCES.getText();
+                generator.toolBox.SOURCES = tfSOURCES.getText();
+                System.out.println("poop4");
+                generator.effect1 = cbEffect1.isSelected();
+                generator.effect2 = cbEffect2.isSelected();
+                generator.effect3 = cbEffect3.isSelected();
+                generator.effect4 = cbEffect4.isSelected();
+                generator.effect5 = cbEffect5.isSelected();
+                generator.effect6 = cbEffect6.isSelected();
+                generator.effect7 = cbEffect7.isSelected();
+                generator.effect8 = cbEffect8.isSelected();
+                generator.effect9 = cbEffect9.isSelected();
+                generator.effect10 = cbEffect10.isSelected();
+                generator.effect11 = cbEffect11.isSelected();
+                generator.insertTransitionClips = cbUseTransitions.isSelected();
+                
+                System.out.println("poop5");
+                for (String source : sourceList) {
+                    generator.addSource("\"" + source + "\"");
+                }
+                System.out.println("poop6");
+                int maxclips = Integer.parseInt(tfClipCount.getText());
+                generator.setMaxClips(Integer.parseInt(tfClipCount.getText()));
+                generator.setMaxDuration(Double.parseDouble(tfMaxStream.getText()));
+                generator.setMinDuration(Double.parseDouble(tfMinStream.getText()));
+                System.out.println("poop7");
+                
+                double timeStarted = System.nanoTime();
+                double elapsedTime = System.nanoTime() - timeStarted;
+
+                
+                generator.go();
+                System.out.println("poop8");
+                while (!generator.done) {
+                    barProgress.setProgress(generator.doneCount);
+
+                //    System.out.println((elapsedTime * generator.doneCount ) - elapsedTime);
+                }
+                barProgress.setProgress(1);
+                try {
+                mediaviewVideoPlayer.getMediaPlayer().stop();
+                } catch (Exception ex) {} //if there's no video then don't bother doing anything
+                
+                Thread.sleep(1000);
+                File media = new File(tfTEMP.getText() + "tempoutput.mp4");
+                //System.out.println("AAAAAAAAAAAAAAA" + media.toURL().toString());
+                Media m = new Media(media.toURI().toString());
+                MediaPlayer mm = new MediaPlayer(m);
+                mediaviewVideoPlayer.setMediaPlayer(mm);
+                btnCreate.setDisable(false);
+                } catch (Exception ex) {
+                   
+                   btnCreate.setDisable(false);
+                   ex.printStackTrace();
+                }
+            }
+        };
+        vidThread.start();
+    }
+
+    @FXML
+    void helpMeConfig(ActionEvent event) {
+        alert("These boxes provide the paths to the executables "
+                + "which will be run throughout the batch process.\n\n"
+                + "If you're on linux or OSX, \"magick\" should be empty.\n\n"
+                + "The only reason it is here is because on Windows, most "
+                + "ImageMagick tools are called from commandline by using "
+                + "\"magick convert...\", while on other operating systems, "
+                + "magick commands can be called simply by saying \"convert...\".\n\n"
+                + "If you have separate installations of these tools, feel "
+                + "free to change what's in these boxes to suit your fancy.\n\n"
+                + "If you have no idea what any of this shit means, leave it be."
+                );
+
+    }
+    
+    @FXML
+    void helpMeEffect(ActionEvent event) {
+        alert("Currently, these effects are based on a switch statement, and each effect has an equal chance of appearing, which means if you "
+                + "turn one of them off, there will be more unedited clips. Additionally, there is a 1/2 chance of there even being an effect on a clip. "
+                + "You do the math. There's 11 effects. 1/2 chance of each effect occuring. That means, regardless of being turned on or off, "
+                + "each effect has a 1/22 chance of occuring. Pretty nasty, right? I'll add sliders for \"frequency\" in the future...\n\n"
+                + "This might be beneficial to know also: There's a 1/15 chance of a \"transition\" clip being used in place of your sources, too. "
+                + "So for every 15 clips you tell YTP+ to generate, one of them will be a transition clip from the folder you provide the program. "
+                + "It's a big mess of numbers.\n\n"
+                + "The reason there aren't frequency sliders now is because someone will probably break them somehow and I don't have the time to debug. "
+                + "Give me a few weeks for an update..."
+                );
+
+    }
+    
+    @FXML
+    void openBrowser(ActionEvent event) {
+        switch (((Control)event.getSource()).getId()) {
+            case "btnBrowseFFMPEG":
+                actuallyOpenBrowser(tfFFMPEG);
+                break;
+            case "btnBrowseFFPROBE":
+                actuallyOpenBrowser(tfFFPROBE);
+                break;
+            case "btnBrowseMAGICK":
+                actuallyOpenBrowser(tfMAGICK);
+                break;
+            case "btnBrowseTEMP":
+                actuallyOpenDirBrowser(tfTEMP);
+                break;
+            case "btnBrowseSOUNDS":
+                actuallyOpenDirBrowser(tfSOUNDS);
+                break;
+            case "btnBrowseMUSIC":
+                actuallyOpenDirBrowser(tfMUSIC);
+                break;
+            case "btnBrowseRESOURCES":
+                actuallyOpenDirBrowser(tfRESOURCES);
+                break;
+            case "btnBrowseSOURCES":
+                actuallyOpenDirBrowser(tfSOURCES);
+                break;
+        }
+    }
+    
+    public File LAST_BROWSED;
+    
+    void actuallyOpenBrowser(TextField toChange) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose File");
+        fileChooser.setInitialDirectory(LAST_BROWSED);
+        File selected = fileChooser.showOpenDialog(null);
+        if (selected==null) return;
+        toChange.setText(selected.getAbsolutePath().replace('\\', '/'));
+        LAST_BROWSED = selected.getParentFile();
+    }
+    
+    void actuallyOpenDirBrowser(TextField toChange) {
+        DirectoryChooser fileChooser = new DirectoryChooser();
+        fileChooser.setTitle("Choose File");
+        fileChooser.setInitialDirectory(LAST_BROWSED);
+        File selected = fileChooser.showDialog(null);
+        if (selected==null) return;
+        toChange.setText(selected.getAbsolutePath().replace('\\', '/') + "/");
+        LAST_BROWSED = selected.getParentFile();
+    }
+
+    
+    //MediaPlayer player = new MediaPlayer(m);
+    
+    @FXML
+    void pauseTheVideo(ActionEvent event) {
+        try {
+        mediaviewVideoPlayer.getMediaPlayer().pause();
+        } catch (Exception ex) {} //if there's no video then don't bother doing anything
+    }
+
+    @FXML
+    void playTheVideo(ActionEvent event) {
+        try {
+        mediaviewVideoPlayer.getMediaPlayer().play();
+        } catch (Exception ex) {} //if there's no video then don't bother doing anything
+    }
+
+    @FXML
+    void removeSource(ActionEvent event) {
+        sourceList.remove(listviewSourcesList.getSelectionModel().getSelectedItems().get(0));
+    }
+
+    @FXML
+    void restartTheVideo(ActionEvent event) {
+        try {
+        mediaviewVideoPlayer.getMediaPlayer().stop();
+        } catch (Exception ex) {} //if there's no video then don't bother doing anything
+    }
+
+    @FXML
+    void saveAsVideo(ActionEvent event) {
+        if (!new File(tfTEMP.getText() + "tempoutput.mp4").exists()) return;
+        FileChooser.ExtensionFilter fileFilter = new FileChooser.ExtensionFilter("Video files (*.mp4)", "*.mp4");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(fileFilter);
+        fileChooser.setTitle("Choose Source");
+        fileChooser.setInitialDirectory(LAST_BROWSED);
+        File selected = fileChooser.showSaveDialog(null);
+        if (selected==null) return;
+        Path temp = Paths.get(tfTEMP.getText() + "tempoutput.mp4");
+        try {
+        Files.copy(temp, selected.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception ex) {
+            alert("Had a problem copying the file.");
+        }
+    }
+    
+    @FXML
+    void openDiscord(ActionEvent event) {
+        try {
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            Desktop.getDesktop().browse(new URI("https://discord.gg/mAwQQt7"));
+        }
+        } catch (Exception ex) {} //how does that even happen
+    }
+
+    @FXML
+    void openArcticZone() {
+        try {
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            Desktop.getDesktop().browse(new URI("https://arctic.zone/"));
+        }
+        } catch (Exception ex) {} //how does that even happen
+    }
+    
+    void alert(String message) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        String[] titles = {"Yo", "Mmmmm!", "I'm the invisible man...", "Luigi, look!", "You want it?", "WTF Booooooooooom"};
+        alert.setTitle(titles[randomInt(0,titles.length-1)]);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+
+        alert.show();
+    }
+    
+    public static int randomInt(int min, int max) {
+        return new Random().nextInt((max - min) + 1) + min;
+    }
+}
---
+++ library/src/main/java/zone/arctic/ytpplus/YTPGenerator.java
@@ -0,0 +1,272 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zone.arctic.ytpplus;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class YTPGenerator {
+
+    public static double MAX_STREAM_DURATION = 0.4; //default: 2s
+    public static double MIN_STREAM_DURATION = 0.2; //default: 0.2s
+    public static int MAX_CLIPS = 20; //default: 5 clips
+    public static String INPUT_FILE; //Input video file
+    public static String OUTPUT_FILE; //the video file that will be produced in the end
+    
+    public boolean effect1;
+    public boolean effect2;
+    public boolean effect3;
+    public boolean effect4;
+    public boolean effect5;
+    public boolean effect6;
+    public boolean effect7;
+    public boolean effect8;
+    public boolean effect9;
+    public boolean effect10;
+    public boolean effect11;
+    public boolean insertTransitionClips;
+    
+    public Utilities toolBox = new Utilities();
+    
+    public void configurate() {
+        //add some code to load this from a .cfg file later
+        toolBox.FFMPEG = "ffmpeg";
+        toolBox.FFPROBE = "ffprobe";
+        toolBox.MAGICK = "magick ";
+        toolBox.TEMP = "temp/" + "job_" + System.currentTimeMillis() + "/";
+        new File(toolBox.TEMP).mkdir();
+        toolBox.SOURCES = "assets/sources/";
+        toolBox.SOUNDS = "assets/sounds/";
+        toolBox.MUSIC = "assets/music/";
+        toolBox.RESOURCES = "resources/";
+    
+        effect1=true;
+        effect2=true;
+        effect3=true;
+        effect4=true;
+        effect5=true;
+        effect6=true;
+        effect7=true;
+        effect8=true;
+        effect9=true;
+        effect10=true;
+        effect11=true;
+        
+        insertTransitionClips=true;
+    }
+    
+    EffectsFactory effectsFactory = new EffectsFactory(toolBox);
+    ArrayList<String> sourceList = new ArrayList<String>();
+    public volatile boolean done = false;
+    public volatile double doneCount = 0;
+    
+    public YTPGenerator(String output) {
+        this.OUTPUT_FILE = output;
+        //configurate();
+    }
+    
+    public YTPGenerator(String output, double min, double max) {
+        this.OUTPUT_FILE = output;
+        this.MIN_STREAM_DURATION = min;
+        this.MAX_STREAM_DURATION = max;
+        //configurate();
+    }
+    public YTPGenerator(String output, double min, double max, int maxclips) {
+        this.OUTPUT_FILE = output;
+        this.MIN_STREAM_DURATION = min;
+        this.MAX_STREAM_DURATION = max;
+        this.MAX_CLIPS = maxclips;
+        //configurate();
+    }
+    
+    public void setMaxClips(int clips) {
+        this.MAX_CLIPS = clips;
+    }
+    public void setMinDuration(double min) {
+        this.MIN_STREAM_DURATION = min;
+    }
+    public void setMaxDuration(double max) {
+        this.MAX_STREAM_DURATION = max;
+    }
+    
+    public void addSource(String sourceName) {
+        sourceList.add(sourceName);
+    }
+
+    public void go() {
+        System.out.println("My FFMPEG is: " + toolBox.FFMPEG);
+        System.out.println("My FFPROBE is: " + toolBox.FFPROBE);
+        System.out.println("My MAGICK is: " + toolBox.MAGICK);
+        System.out.println("My TEMP is: " + toolBox.TEMP);
+        System.out.println("My SOUNDS is: " + toolBox.SOUNDS);
+        System.out.println("My SOURCES is: " + toolBox.SOURCES);
+        System.out.println("My MUSIC is: " + toolBox.MUSIC);
+        System.out.println("My RESOURCES is: " + toolBox.RESOURCES);
+        Thread vidThread = new Thread() {
+            public void run() {
+                if (sourceList.isEmpty()) {
+                    System.out.println("No sources added...");
+                    return;
+                }
+                
+                System.out.println("poop_1");
+                File out = new File(OUTPUT_FILE);
+                if (out.exists()) {
+                    out.delete();
+                }
+                cleanUp();
+
+                try {
+                    PrintWriter writer = new PrintWriter(toolBox.TEMP+"concat.txt", "UTF-8");
+                    for (int i = 0; i < MAX_CLIPS; i++) {
+                        doneCount = (double) i/MAX_CLIPS;
+                        String sourceToPick = sourceList.get(randomInt(0, sourceList.size() - 1));
+                        System.out.println(sourceToPick);
+                        System.out.println(toolBox.getLength(sourceToPick));
+                        TimeStamp boy = new TimeStamp(Double.parseDouble(toolBox.getLength(sourceToPick)));
+                        System.out.println(boy.getTimeStamp());
+                        System.out.println("STARTING CLIP " + "video" + i);
+                        TimeStamp startOfClip = new TimeStamp(randomDouble(0.0, boy.getLengthSec() - MAX_STREAM_DURATION));
+                        //System.out.println("boy seconds = "+  boy.getLengthSec());
+                        TimeStamp endOfClip = new TimeStamp(startOfClip.getLengthSec() + randomDouble(MIN_STREAM_DURATION, MAX_STREAM_DURATION));
+                        System.out.println("Beginning of clip " + i + ": " + startOfClip.getTimeStamp());
+                        System.out.println("Ending of clip " + i + ": " + endOfClip.getTimeStamp() + ", in seconds: ");
+                        if (randomInt(0, 15) == 15 && insertTransitionClips==true) {
+                            System.out.println("Tryina use a diff source");
+                            toolBox.copyVideo(toolBox.SOURCES + effectsFactory.pickSource(), toolBox.TEMP+"video" + i);
+                        } else {
+                            toolBox.snipVideo(sourceToPick, startOfClip, endOfClip, toolBox.TEMP+"video" + i);
+                        }
+                        //Add a random effect to the video
+                        int effect = randomInt(0, 16);
+                        System.out.println("STARTING EFFECT ON CLIP " + i + " EFFECT" + effect);
+                        String clipToWorkWith = toolBox.TEMP+"video" + i + ".mp4";
+                        switch (effect) {
+                            case 1:
+                                //random sound
+                                if (effect1==true)
+                                effectsFactory.effect_RandomSound(clipToWorkWith);
+                                break;
+                            case 2:
+                                if (effect2==true)
+                                //random sound
+                                effectsFactory.effect_RandomSoundMute(clipToWorkWith);
+                                break;
+                            case 3:
+                                if (effect3==true)
+                                effectsFactory.effect_Reverse(clipToWorkWith);
+                                break;
+                            case 4:
+                                if (effect4==true)
+                                effectsFactory.effect_SpeedUp(clipToWorkWith);
+                                break;
+                            case 5:
+                                if (effect5==true)
+                                effectsFactory.effect_SlowDown(clipToWorkWith);
+                                break;
+                            case 6:
+                                if (effect6==true)
+                                effectsFactory.effect_Chorus(clipToWorkWith);
+                                break;
+                            case 7:
+                                if (effect7==true)
+                                effectsFactory.effect_Vibrato(clipToWorkWith);
+                                break;
+                            case 8:
+                                if (effect8==true)
+                                effectsFactory.effect_HighPitch(clipToWorkWith);
+                                break;
+                            case 9:
+                                if (effect9==true)
+                                effectsFactory.effect_LowPitch(clipToWorkWith);
+                                break;
+                            case 10:
+                                if (effect10==true)
+                                effectsFactory.effect_Dance(clipToWorkWith);
+                                break;
+                            case 11:
+                                if (effect11==true)
+                                effectsFactory.effect_Squidward(clipToWorkWith);
+                                break;
+                            default:
+                                break;
+                        }
+
+                    }
+                    for (int i = 0; i < MAX_CLIPS; i++) {
+                        if (new File(toolBox.TEMP+"video" + i + ".mp4").exists()) {
+                            writer.write("file 'video" + i + ".mp4'\n"); //writing to same folder
+                        }
+                    }
+                    writer.close();
+                    //Thread.sleep(10000);
+                    toolBox.concatenateVideo(MAX_CLIPS, OUTPUT_FILE);
+                    //Thread.sleep(4000);
+
+                } catch (Exception ex) { ex.printStackTrace();
+                }
+                //for (int i=0; i<100; i++) {
+                cleanUp();
+                rmDir(new File(toolBox.TEMP));
+                done = true;
+            }
+        };
+        vidThread.start();
+        
+    }
+        
+    
+    public boolean isDone() {
+        return done;
+    }
+    
+    public static double randomDouble(double min, double max) {
+        double finalVal = -1;
+        while (finalVal<0) {
+            double x = (Math.random() * ((max - min) + 0)) + min;
+            finalVal=Math.round(x * 100.0) / 100.0; 
+        }
+        return finalVal;
+    }
+    
+    public static int randomInt(int min, int max) {
+        return new Random().nextInt((max - min) + 1) + min;
+    }
+    
+    
+    public void cleanUp() {
+        //Create concatenation text file
+        File text = new File(toolBox.TEMP+"concat.txt");
+        if (text.exists())
+            text.delete();
+        File mp4 = new File(toolBox.TEMP + "temp.mp4");
+        if (mp4.exists())
+            mp4.delete(); 
+        for (int i=0; i<MAX_CLIPS; i++) {
+            File del = new File(toolBox.TEMP + "video"+i+".mp4");
+            if (del.exists()) {
+                System.out.println(i + " Exists");
+                del.delete();
+            }
+        }
+
+    }
+    public void rmDir(File file) {
+        File[] contents = file.listFiles();
+        if (contents != null) {
+            for (File f : contents) {
+                if (! Files.isSymbolicLink(f.toPath())) {
+                    rmDir(f);
+                }
+            }
+        }
+        file.delete();
+    }
+    
+}
